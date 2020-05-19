ae2193c743ca0529c5556efffaba8d285b11d718
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-05-19 22:00:41 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,4 +5,12 @@ original_array.each do |string|
   return_array << string.reverse
 end
 return_array.join(" ")
-  
+
+def reverse_each_word(string)
+  array = string.split(" ")
+  test_array = []
+  array.collect do |string|
+    test_array << string.reverse
+  end
+  test_array.join(" ")
+end

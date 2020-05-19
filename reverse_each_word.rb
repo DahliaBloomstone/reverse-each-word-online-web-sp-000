b68c8a541dfffc2110b43461222def779a58e2b8
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-05-19 22:01:16 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,10 @@
-def reverse_each_word(string)
-original_array = string.split(" ")
-return_array = []
-original_array.each do |string|
-  return_array << string.reverse
-end
-return_array.join(" ")
+#def reverse_each_word(string)
+#original_array = string.split(" ")
+#return_array = []
+#original_array.each do |string|
+#  return_array << string.reverse
+#end
+#return_array.join(" ")
 
 def reverse_each_word(string)
   array = string.split(" ")
